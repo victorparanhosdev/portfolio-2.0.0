15197ca9272b2fc0319088a0474848eeccf2400c
Victor Paranhos Costa <vpcosta@latam.stefanini.com>
2024-08-18 19:25:22 -0300

    Ajustar Responsividade

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -31,10 +31,11 @@ export const Skills = () => {
                         modules={[Pagination, Autoplay, Mousewheel, Navigation, A11y, EffectCoverflow]}
                         pagination={{ clickable: true, dynamicBullets: true }}
                         effect={'coverflow'}
-                        autoplay={{ delay: 1400 }}
+                        autoplay={{ delay: 1200 }}
                         mousewheel={{ invert: true }}
                         grabCursor={true}
                         centeredSlides={true}
+                        loop
                         breakpoints={{
                             0: {
                                 slidesPerView: 1.5,
@@ -71,10 +72,12 @@ export const Skills = () => {
                     </Swiper> :
                     <ul className="grid gap-4 grid-cols-skills">
                         {
-                            iconsSkills.map((item) => {
+                            iconsSkills.map((item, index) => {
                                 const Icon = item.imagem
+                                const formattedString = index > 9 ? `${index}00` : `${index}00`;
+     
                                 return (
-                                    <li key={item.tech}
+                                    <li data-aos="flip-up" data-aos-duration={formattedString} key={item.tech}
                                         className=" rounded-xl grid justify-items-center justify-center content-center dark:bg-gray-dark-200  min-h-[120px] dark:border-0 border bg-[#f0f6ff80] border-[#2c96df78] text-blue-light-100 dark:text-gray-dark-300 group text-center transition-colors group ">
                                         <h1 className="text-center mb-2 font-bold text-[#1247a5] dark:text-[#8fbfd9] opacity-80 group-hover:opacity-100">{item.tech}</h1>
                                         <Icon className="text-5xl text-center w-full text-[#1247a5] dark:text-[#8fbfd9] opacity-90 group-hover:opacity-100" />
