b6c4539161c7489211d83e14d6c850515c5c9cc8
Victor Paranhos <victor_paranhos@hotmail.com>
2024-08-19 00:58:55 -0300

    Atualizar o loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,9 +2,9 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function Loading () {
     return (
-        <div className="h-screen w-full dark:bg-gray-dark-500 bg-white flex justify-center items-center">
+        <div className="min-h-screen w-full dark:bg-gray-dark-500 bg-white flex justify-center items-center">
             <div className="overflow-hidden">
-              <AiOutlineLoading3Quarters className="animate-spin text-4xl  text-blue-light-200 dark:text-blue-dark-100"/>
+              <AiOutlineLoading3Quarters className="animate-spin text-5xl  text-blue-light-200 dark:text-blue-dark-100"/>
             </div>
         </div>
     )
