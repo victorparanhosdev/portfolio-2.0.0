238279b1d8546177a870c8f1251c24b0ed00ad43
Victor Paranhos <victor_paranhos@hotmail.com>
2024-09-20 21:39:16 -0300

    Atualizar o page.tsx

--- src/app/[locale]/projetos/page.tsx
+++ src/app/[locale]/projetos/page.tsx
@@ -132,7 +132,7 @@ export default function AllProjetos() {
                             cursor: 'bg-blue-light-200  dark:bg-blue-dark-100 dark:text-gray-dark-500',
 
                             
-                        }} showControls size="lg" showShadow page={currentPage} initialPage={1} radius='md' total={TotalPages} onChange={setCurrentPage} />
+                        }} showControls boundaries={3} size="lg" showShadow page={currentPage} initialPage={1} radius='md' total={TotalPages} onChange={setCurrentPage} />
                     </div>
                 }
 
